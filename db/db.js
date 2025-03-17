08f621f89017882253c1d02937f8b71963ecd6ca
huntdavid175 <huntdavid175@gmail.com>
2025-03-17 18:09:39 -0300

    updated gitignore file

--- db/db.js
+++ db/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  user: "fawaz", // replace with your database username
-  host: "localhost",
-  database: "meal_kit_db", // your database name
-  password: "", // replace with your database password
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool.on("connect", () => {
